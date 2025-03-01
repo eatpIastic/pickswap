032519485b41a0f78474aebc4c75d3ac93c6d31f
eatplastic <117698474+eatpIastic@users.noreply.github.com>
2025-02-28 21:43:23 -0600

    decimals command, tab completion

--- config.js
+++ config.js
@@ -7,77 +7,84 @@ export const data = new PogObject("pickswap", {
     "streakpb": 0,
     "sound": "random.orb",
     "show": true,
-    "rx": 4,
-    "ry": 4,
-    "pb": [10000,10000,10000,10000,10000,10000,10000,10000,10000,10000,10000,10000,10000,10000,10000,10000,10000,10000,10000,10000,10000]
+    "pb": [10000,10000,10000,10000,10000,10000,10000,10000,10000,10000,10000,10000,10000,10000,10000,10000,10000,10000,10000,10000,10000],
+    "decimals": 1,
+    "x": 100,
+    "y": 100
 }, "data.json");
 
 export const show = () => {return data.show};
+const tabCompletions = ["setsound", "reset", "spam", "streak", "ding", "show", "move", "decimals"];
 
 const gui = new Gui();
 
 register("command", (...args) => {
-  if(!args) args = [""]
-  switch(args[0].toLowerCase()) {
-    case "reset":
-      for(let i=0; i<data.pb.length; i++) {
-        data.pb[i] = 10000;
-      }
-      data.streakpb = 0;
-      data.save();
-      ChatLib.chat("pickswap pb data reset");
-      break;
-    case "spam":
-      data.spam = !data.spam;
-      data.save();
-      ChatLib.chat(`pickswap set to${data.spam ? " " : " not "}spam`);
-      break;
-    case "streak":
-      data.streak = !data.streak;
-      data.save();
-      ChatLib.chat(`pickswap streak ${data.streak ? "enabled" : "disabled"}`);
-      break;
-    case "show":
-      data.show = !data.show;
-      data.save();
-      ChatLib.chat(`pickswap gui set to${data.show ? "" : " not"} show`);
-      break;
-    case "move":
-      gui.open();
-      break;
-    case "ding":
-      data.ding = !data.ding;
-      data.save();
-      ChatLib.chat(`pickswap ding ${data.ding ? "enabled" : "disabled"}`);
-      break;
-    case "getr":
-      ChatLib.chat("formatted as x, y. copy the chat msg and just paste the whole thing into another module with setr comma and all. or without the comma. it doesnt matter.");
-      ChatLib.chat(`${data.rx} ${data.ry}`);
-      break;
-    case "setr":
-      data.rx = args[1].replace(",", "").replace(" ", "");
-      data.ry = args[2].replace(" ", "");
-      data.save();
-      break;
-    case "setsound":
-      data.sound = args[1];
-      try {
-        World.playSound(args[1], 1, 1);
-      } catch(e) {
-        ChatLib.chat("pickswap sound error. reset to random.orb");
-        data.sound = "random.orb";
-      }
-      data.save();
-      break;
-    default:
-      ChatLib.chat("pickswap cmds: reset, spam, streak, ding, show, move, getr, setr");
-      break;
-  }
-}).setName("pickswap");
+    if (!args?.[0]) {
+        ChatLib.chat(`pickswap cmds: ${tabCompletions.join(", ")}`);
+        return;
+    }
 
-register("dragged", (dx, dy, x, y, b) => {
-    if(!gui.isOpen()) return;
-    data.rx = (x/Renderer.screen.getWidth());
-    data.ry = (y/Renderer.screen.getHeight());
+    switch(args[0].toLowerCase()) {
+        case "reset":
+            for(let i=0; i<data.pb.length; i++) {
+                data.pb[i] = 10000;
+            }
+            data.streakpb = 0;
+            data.save();
+            ChatLib.chat("pickswap pb data reset");
+            break;
+        case "spam":
+            data.spam = !data.spam;
+            data.save();
+            ChatLib.chat(`pickswap set to${data.spam ? " " : " not "}spam`);
+            break;
+        case "streak":
+            data.streak = !data.streak;
+            data.save();
+            ChatLib.chat(`pickswap streak ${data.streak ? "enabled" : "disabled"}`);
+            break;
+        case "show":
+            data.show = !data.show;
+            data.save();
+            ChatLib.chat(`pickswap gui set to${data.show ? "" : " not"} show`);
+            break;
+        case "move":
+            gui.open();
+            break;
+        case "ding":
+            data.ding = !data.ding;
+            data.save();
+            ChatLib.chat(`pickswap ding ${data.ding ? "enabled" : "disabled"}`);
+            break;
+        case "setsound":
+            data.sound = args[1];
+            try {
+                World.playSound(args[1], 1, 1);
+            } catch(e) {
+                ChatLib.chat("pickswap sound error. reset to random.orb");
+                data.sound = "random.orb";
+            }
+            data.save();
+            break;
+        case "decimals":
+            let tempDecimals = parseInt(args?.[1]);
+            if (!tempDecimals) {
+                ChatLib.chat("ex: /pickswap decimals 2");
+                return;
+            }
+            data.decimals = tempDecimals;
+            data.save();
+            break;
+    }
+}).setName("pickswap").setTabCompletions(tabCompletions);
+
+
+register("dragged", (dx, dy, mx, my, b) => {
+    if(!gui.isOpen()) {
+        return;
+    }
+
+    data.x = mx;
+    data.y = my;
     data.save();
 });
--- index.js
+++ index.js
@@ -11,75 +11,84 @@ let slot = 0;
 let broken = 0;
 let successfulSwaps = [];
 
+if (!("decimals" in data)) {
+    data.decimals = 1;
+    if (!("x" in data)) {
+        data.x = 100;
+    }
+    if (!("y" in data)) {
+        data.y = 100;
+    }
+    data.save();
+}
+
 
 register("packetSent", (packet, event) => {
-  slot = packet.func_149614_c();
-  broken = 0;
+    slot = packet.func_149614_c();
+    broken = 0;
 }).setFilteredClass(C09PacketHeldItemChange);
 
 
 register("blockBreak", () => {
-  if(broken>=1) return;
-  broken++;
-
-  let packetItem = Player.getInventory()?.getItems()[slot]?.getRegistryName();
-  let holdingPickaxe = Player.getHeldItem()?.getRegistryName()?.includes("pickaxe");
-
-  if(data.streak && packetItem?.includes("pickaxe") && holdingPickaxe) {
-    ChatLib.chat(`&cStreak Broken: ${row}`);
-    if(row>=data.streakpb) {
-      ChatLib.chat(`new pickswap streak pb: ${row}`);
-      data.streakpb = row;
-      data.save();
+    if (broken >= 1) {
+        return;
     }
-    row = 0;
-  }
-  if(!packetItem?.includes("pickaxe") && packetItem!="minecraft:bow") {
-    if(holdingPickaxe) {
-      row++;
-      if(data.ding) World.playSound(data.sound, 1, 1);
-      let t = Date.now();
-      successfulSwaps.push(t);
-      onSwap(t);
+
+    broken++;
+
+    let packetItem = Player.getInventory()?.getItems()[slot]?.getRegistryName();
+    let holdingPickaxe = Player.getHeldItem()?.getRegistryName()?.includes("pickaxe");
+
+    if (data.streak && packetItem?.includes("pickaxe") && holdingPickaxe) {
+        ChatLib.chat(`&cStreak Broken: ${row}`);
+        if (row >= data.streakpb) {
+        ChatLib.chat(`new pickswap streak pb: ${row}`);
+        data.streakpb = row;
+        data.save();
+        }
+        row = 0;
     }
-  }
+    if (!packetItem?.includes("pickaxe") && packetItem != "minecraft:bow") {
+        if (holdingPickaxe) {
+            row++;
+            if(data.ding) World.playSound(data.sound, 1, 1);
+            let t = Date.now();
+            successfulSwaps.push(t);
+            onSwap(t);
+        }
+    }
 });
 
 
 registerWhen(register("renderOverlay", () => {
-  if(avg==0) return;
-  let str = `&7BPS: &f${(1000/avg).toFixed(1)}`;
-  const w = Renderer.screen.getWidth();
-  let x = w * data.rx;
-  let sw = Renderer.getStringWidth(str);
-  if(x+sw>w) {
-    x = w-sw;
-    data.rx = (x/w);
-    data.save();
-  }
-  Renderer.drawStringWithShadow(str, Renderer.screen.getWidth() * data.rx, Renderer.screen.getHeight() * data.ry);
+    if(avg == 0) {
+        return;
+    }
+
+    let str = `&7BPS: &f${(1000 / avg).toFixed(data.decimals)}`;
+    Renderer.drawStringWithShadow(str, data.x, data.y);
 }), show);
 
 
 function onSwap(t) {
-  successfulSwaps = successfulSwaps.filter(n => (t-n)<=1000);
+    successfulSwaps = successfulSwaps.filter(n => (t - n) <= 1000);
 
-  if(successfulSwaps.length<2) {
-    avg = 0;
-    return;
-  }
+    if(successfulSwaps.length < 2) {
+        avg = 0;
+        return;
+    }
 
-  avg = (successfulSwaps[successfulSwaps.length-1]-successfulSwaps[0]) / successfulSwaps.length;
-  const beatsPB = data.pb[successfulSwaps.length]>=avg;
+    avg = (successfulSwaps[successfulSwaps.length - 1] - successfulSwaps[0]) / successfulSwaps.length;
+    const beatsPB = data.pb[successfulSwaps.length] >= avg;
 
-  if(!beatsPB && !data.spam) return;
+    if(!beatsPB && !data.spam) return;
 
-  ChatLib.chat(`${beatsPB ? "&l&cPB! >>> " : ""}&f${successfulSwaps.length}: &a${avg} &f(&c${data.pb[successfulSwaps.length]}&f)`);
-  if(beatsPB) {
-    data.pb[successfulSwaps.length] = avg;
-    data.save();
-  }
-  if(successfulSwaps.length >= 5 && avg <= 100) {
-    ChatLib.chat(`&c>>  &f${successfulSwaps.length}: ${avg}`);
-  }
+    ChatLib.chat(`${beatsPB ? "&l&cPB! >>> " : ""}&f${successfulSwaps.length}: &a${avg.toFixed(data.decimals)} &f(&c${data.pb[successfulSwaps.length]}&f)`);
+    if(beatsPB) {
+        data.pb[successfulSwaps.length] = avg;
+        data.save();
+    }
+    if(successfulSwaps.length >= 5 && avg <= 100) {
+        ChatLib.chat(`&c>>  &f${successfulSwaps.length}: ${avg.toFixed(data.decimals)}`);
+    }
 }
